fadc2237becc936734cb90fc13613760a24a7092
quzik <mezz60606@gmail.com>
2026-04-11 00:12:20 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request) {
     return new Response(`<!DOCTYPE html>
-    <!-- paste your FULL HTML here -->`, {
+    <!-- paste your full HTML here -->`, {
       headers: { "content-type": "text/html;charset=UTF-8" },
     });
   },
